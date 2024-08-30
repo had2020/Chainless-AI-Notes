e87e0340efc8f9d6e81a34848e7dbbaf79e928b9
Hadrian Lazic <hadrian.lazic@gmail.com>
2024-08-30 14:12:04 -0700

    updates1

--- main.py
+++ main.py
@@ -2,52 +2,19 @@ from flask import Flask, render_template, send_file, request , jsonify
 from flask_cors import CORS
 import os
 from werkzeug.utils import secure_filename
-from flask_socketio import SocketIO, emit
-import websockets
 from flask_assets import Environment, Bundle
-import sounddevice as sd
-import numpy as np
+from pvrecorder import PvRecorder
 
 app = Flask(__name__)
 # needed for packs use
 CORS(app)
-socketio = SocketIO(app)
-assets = Environment(app)
 
-# Setting up uploads folder
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+devices = PvRecorder.get_available_devices()
+recorder = PvRecorder(frame_length=512, device_index=0)
 
-# Define bundles
-js_bundle = Bundle('js/script1.js', 'js/script2.js', output='dist/bundle.js')
-css_bundle = Bundle('css/style.css', output='dist/style.css')
-assets.register('js_all', js_bundle)
-assets.register('css_all', css_bundle)
-
-# check the upload folder exists
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# Audio recording functionality
-recording = False
-audio_data = np.random.rand(10000)  # Example: Random noise
-
-# Set up audio parameters
-samplerate = 44100
-channels = 1
-
-# Create a continuous playback stream
-stream = sd.OutputStream(samplerate=samplerate, channels=channels)
-stream.start()
-
-# Start the while loop
-while recording == True:
-    # Play the audio data
-    stream.write(audio_data)
+while recorder.is_recording:
+    frame = recorder.read()
+    # process audio frame
 
 # Flask routes
 @app.route('/')
@@ -56,7 +23,14 @@ def index():
 
 @app.route('/process', methods=['POST', 'GET'])
 def process():
-    recording = True # TODO globalifty this
+    global recording
+    print(recording)
+    recording = not recording 
+    if recording == False:
+        print("Recording stopped")
+        recorder.stop()
+    while recording == True:
+        recorder.start()
     return render_template('index.html')
 
 if __name__ == '__main__':
